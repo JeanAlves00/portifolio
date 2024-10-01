dc4982c53d2005bb9b53860ffc1541bab99a604c
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-10-01 10:45:44 -0400

    teste

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-    const cabecalho = document.querySelector('.cabecalho');
-
-    menuToggle.addEventListener('click', () => {
-        cabecalho.classList.toggle('active'); // Alterna a classe 'active' para mostrar ou ocultar o menu
-    });
