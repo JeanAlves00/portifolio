82e75fc354e3f3c913d8886c42c39bb93914c42f
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-10-02 10:01:03 -0400

    recomeço

---
+++ dist/index.f75de5e1.js
@@ -0,0 +1,16 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+burger.addEventListener("click", ()=>{
+    navLinks.classList.toggle("active");
+    burger.classList.toggle("active"); // Alterna o estado do ícone de fechar
+});
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", ()=>{
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) navbar.style.top = "-80px"; // Esconde a navbar
+    else navbar.style.top = "0";
+    lastScrollTop = scrollTop;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const menu = document.querySelector(".cabecalho");
-
-    menuToggle.addEventListener("click", function() {
-        menu.classList.toggle("active"); // Alterna a classe 'active' no menu
-    });
-});
