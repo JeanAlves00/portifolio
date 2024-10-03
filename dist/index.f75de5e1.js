c6a1ffda2dc2a0267819d2ee3e09d1e449e23546
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-10-03 14:25:00 -0400

    pt

--- dist/index.f75de5e1.js
+++ dist/index.f75de5e1.js
@@ -1,16 +1,38 @@
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
-burger.addEventListener("click", ()=>{
-    navLinks.classList.toggle("active");
-    burger.classList.toggle("active"); // Alterna o estado do ícone de fechar
+//cookie
+document.addEventListener("DOMContentLoaded", ()=>{
+    const cookieBanner = document.getElementById("cookie-banner");
+    const acceptCookiesButton = document.getElementById("accept-cookies");
+    // Verifica se o usuário já aceitou os cookies
+    if (!localStorage.getItem("cookiesAccepted")) setTimeout(()=>{
+        cookieBanner.classList.add("show"); // Adiciona a classe para mostrar o banner
+    }, 1000); // Atraso de 1 segundo antes de mostrar
+    // Ao clicar no botão "Aceitar Cookies"
+    acceptCookiesButton.addEventListener("click", ()=>{
+        localStorage.setItem("cookiesAccepted", "true"); // Armazena a aceitação
+        cookieBanner.classList.remove("show"); // Remove a classe para esconder o banner
+    });
 });
-let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
-window.addEventListener("scroll", ()=>{
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop) navbar.style.top = "-80px"; // Esconde a navbar
-    else navbar.style.top = "0";
-    lastScrollTop = scrollTop;
-});
+//nome que se autoescreve
+const nome = "Jean Pablo Alves Gon\xe7alves";
+let h2Elemento = document.getElementById("typewriter");
+let charIndex = 0;
+let escrevendo = true;
+h2Elemento.innerHTML = nome;
+function typeEffectH2() {
+    if (escrevendo) {
+        h2Elemento.innerHTML = nome.slice(0, charIndex++);
+        if (charIndex > nome.length) {
+            escrevendo = false;
+            setTimeout(typeEffectH2, 1000);
+        } else setTimeout(typeEffectH2, 100); // Velocidade de escrita
+    } else {
+        h2Elemento.innerHTML = nome.slice(0, --charIndex);
+        if (charIndex === 0) {
+            escrevendo = true;
+            setTimeout(typeEffectH2, 500);
+        } else setTimeout(typeEffectH2, 100);
+    }
+}
+typeEffectH2();
 
 //# sourceMappingURL=index.f75de5e1.js.map
